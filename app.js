9264b4ae699d2d35ae24e6421f52860f5096d578
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-03-31 23:49:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// ternary operator
+
+// var student = prompt('kk');
+// var sites;
+
+// (student == 10)? sites = 'yes Avalible' : 'not Avalible';
+
+// console.log(sites)
+
+
+function myDeta(){
+ console.log("name : hussain" + "age : 17" +
+   "class : eleventh"  +
+   "course : WMA" )
+   function gag(){
+    console.log('hay')
+}
+gag()
+}
+
+myDeta()
